f1a90adc61d051a5a9768de17ef6dfb5add8c56f
raheel0316 <raheelanwaar0316@gmail.com>
2023-11-16 10:56:07 -0800

    new changes

--- app/Http/Helper/helper.php
+++ app/Http/Helper/helper.php
@@ -1,10 +1,45 @@
 <?php
 
 use App\Models\User;
+use App\Models\UserWidthraw;
 
 function allUser()
 {
     $user = User::all()->count();
     return $user;
+}
+
+// function pendingUser()
+// {
+//     $user = User::where('status','pending')->count();
+//     return $user;
+// }
+
+// function rejectedUsers()
+// {
+//     $user = User::where('status','rejected')->count();
+//     return $user;
+// }
+
+// function approvedUsers()
+// {
+//     $user = User::where('status','approved')->count();
+//     return $user;
+// }
+
+function investment()
+{
+    return 4;
+}
+
+function widthrawal()
+{
+    $widthrawls = UserWidthraw::where('status','approved')->get();
+    $total_widthraw = 0;
+    foreach($widthrawls as $data)
+    {
+        $total_widthraw += $data->amount;
+    }
+    return $total_widthraw;
 }
 
