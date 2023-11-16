b6fea7e9556a41dee5d0e1b99085473d9cab3382
raheel0316 <raheelanwaar0316@gmail.com>
2023-11-16 12:46:13 -0800

    now it will have recored

--- app/Http/Controllers/user/AdminDashboardController.php
+++ app/Http/Controllers/user/AdminDashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\Recored;
 use App\Models\User;
 use App\Models\UserDeposit;
 use App\Models\UserWidthraw;
@@ -18,39 +19,47 @@ class AdminDashboardController extends Controller
     public function users()
     {
         $users = User::get();
-        return view('admin.user.all',compact('users'));
+        return view('admin.user.all', compact('users'));
     }
 
     public function details($id)
     {
         $user = User::find($id);
-        return view('admin.user.edit',compact('user'));
+        return view('admin.user.edit', compact('user'));
     }
 
-    public function changeDetails(Request $request,$id)
+    public function changeDetails(Request $request, $id)
     {
         $user = User::find($id);
         $user->balance = $request->balance;
         $user->save();
-        return redirect()->back()->with('success','User Balance Updated successfully');
+
+        // storing user data
+        $recored = new Recored();
+        $recored->user_id = $id;
+        $recored->amount = $request->balance;
+        $recored->owner = 'admin';
+        $recored->save();
+
+        return redirect()->back()->with('success', 'User Balance Updated successfully');
     }
 
     public function pendingDeposit()
     {
-        $pending_request = UserDeposit::where('status','pending')->get();
-        return view('admin.deposit.pending',compact('pending_request'));
+        $pending_request = UserDeposit::where('status', 'pending')->get();
+        return view('admin.deposit.pending', compact('pending_request'));
     }
 
     public function approvedDeposit()
     {
-        $pending_request = UserDeposit::where('status','approved')->get();
-        return view('admin.deposit.approved',compact('pending_request'));
+        $pending_request = UserDeposit::where('status', 'approved')->get();
+        return view('admin.deposit.approved', compact('pending_request'));
     }
 
     public function rejectedDeposit()
     {
-        $pending_request = UserDeposit::where('status','rejected')->get();
-        return view('admin.deposit.rejected',compact('pending_request'));
+        $pending_request = UserDeposit::where('status', 'rejected')->get();
+        return view('admin.deposit.rejected', compact('pending_request'));
     }
 
     public function approveRequest($id)
@@ -58,7 +67,7 @@ class AdminDashboardController extends Controller
         $deposit_request = UserDeposit::find($id);
         $deposit_request->status = 'approved';
         $deposit_request->save();
-        return redirect()->back()->with('success','Deposit request approved successfully');
+        return redirect()->back()->with('success', 'Deposit request approved successfully');
     }
 
 
@@ -67,27 +76,27 @@ class AdminDashboardController extends Controller
         $deposit_request = UserDeposit::find($id);
         $deposit_request->status = 'rejected';
         $deposit_request->save();
-        return redirect()->back()->with('success','Deposit request rejected successfully');
+        return redirect()->back()->with('success', 'Deposit request rejected successfully');
     }
 
     // Widthraw Requests
 
     public function pendingWidthraw()
     {
-        $widthraw_requests = UserWidthraw::where('status','pending')->get();
-        return view('admin.widthraw.pending',compact('widthraw_requests'));
+        $widthraw_requests = UserWidthraw::where('status', 'pending')->get();
+        return view('admin.widthraw.pending', compact('widthraw_requests'));
     }
 
     public function approvedWidthraw()
     {
-        $widthraw_requests = UserWidthraw::where('status','approved')->get();
-        return view('admin.widthraw.approved',compact('widthraw_requests'));
+        $widthraw_requests = UserWidthraw::where('status', 'approved')->get();
+        return view('admin.widthraw.approved', compact('widthraw_requests'));
     }
 
     public function rejectedWidthraw()
     {
-        $widthraw_requests = UserWidthraw::where('status','rejected')->get();
-        return view('admin.widthraw.rejected',compact('widthraw_requests'));
+        $widthraw_requests = UserWidthraw::where('status', 'rejected')->get();
+        return view('admin.widthraw.rejected', compact('widthraw_requests'));
     }
 
     public function approveWidthraw($id)
@@ -95,7 +104,7 @@ class AdminDashboardController extends Controller
         $widthraw = UserWidthraw::find($id);
         $widthraw->status = 'approved';
         $widthraw->save();
-        return redirect()->back()->with('success','Widthraw approved successfully');
+        return redirect()->back()->with('success', 'Widthraw approved successfully');
     }
 
     public function rejectWidthraw($id)
@@ -103,7 +112,6 @@ class AdminDashboardController extends Controller
         $widthraw = UserWidthraw::find($id);
         $widthraw->status = 'rejected';
         $widthraw->save();
-        return redirect()->back()->with('success','Widthraw rejected successfully');
+        return redirect()->back()->with('success', 'Widthraw rejected successfully');
     }
-
 }
