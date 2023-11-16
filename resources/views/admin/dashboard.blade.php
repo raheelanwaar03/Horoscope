0ee9287c95cbfc6a7862c53ed260ba95dc4bfbd4
raheel0316 <raheelanwaar0316@gmail.com>
2023-11-16 12:52:32 -0800

    extera remove

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -56,31 +56,6 @@
                                 </div>
                             </div>
                         </div>
-                         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-                            <div class="card  wallet blue">
-                                <div class="boxs">
-                                    <span class="box one"></span>
-                                    <span class="box two"></span>
-                                    <span class="box three"></span>
-                                    <span class="box four"></span>
-                                </div>
-                                <div class="card-header border-0">
-                                    <div class="wallet-info">
-                                        <span class="font-w400 d-block text-white">Given Balance</span>
-                                        <h4 class="fs-24 font-w600 mb-0 d-inline-flex me-2"></h4>
-                                    </div>
-                                    <div class="wallet-icon">
-                                        <svg width="62" height="39" viewBox="0 0 62 39" fill="none"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <circle cx="42.7722" cy="19.2278" r="19.2278" fill="white"
-                                                fill-opacity="0.2" />
-                                            <circle cx="19.2278" cy="19.2278" r="19.2278" fill="white"
-                                                fill-opacity="0.2" />
-                                        </svg>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
                             <div class="card  wallet">
                                 <div class="boxs">
